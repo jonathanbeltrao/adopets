865f0ad63f3e3863601426d60cc78c5d36cd4cca
Rafael Jordão <rafaeljordao.picado@gmail.com>
2022-07-26 22:02:40 -0300

    creating adoption structure

---
+++ database/migrations/2022_07_27_005600_create_adoptions_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAdoptionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('adoptions', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('owner_id')->constrained();
+            $table->foreingId('pet_id')->constrained();
+            $table->foreingId('stackholder_id')->constrained();
+            $table->date('visit_date');
+            $table->date('adoption_date');
+            $table->longText('adoption_date');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('adoptions');
+    }
+}
